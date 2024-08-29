c9e63c10abf5448e39a00b9ea2ffed3a9dc1cc55
VPresich <valya.alexandrova.presich@gmail.com>
2024-08-29 13:47:48 +0200

    Added smooth open accordions in about an FAQ sections

--- src/components/AboutAccordion/AccordionItem/AccordionItem.jsx
+++ src/components/AboutAccordion/AccordionItem/AccordionItem.jsx
@@ -1,15 +1,25 @@
+import { useRef } from "react";
 import css from "./AccordionItem.module.css";
 import AccordionButton from "../../UI/AccordionButton/AccordionButton";
 import clsx from "clsx";
 
 const AccordionItem = ({ title, content, isOpen, onClick }) => {
+  const panelRef = useRef(null);
   return (
     <li className={clsx(css.item, isOpen && css.active)}>
       <h3 className={clsx(css.header)} onClick={onClick}>
         {title}
         <AccordionButton iconId="icon-dropdown" isOpen={isOpen} />
       </h3>
-      <div className={clsx(css.panel, isOpen && css.active)}>
+      <div
+        ref={panelRef}
+        className={clsx(css.panel, isOpen && css.active)}
+        style={{
+          maxHeight: isOpen ? `${panelRef.current?.scrollHeight}px` : "0",
+          opacity: isOpen ? "1" : "0",
+          transition: "max-height 1s ease, opacity 0.5s ease",
+        }}
+      >
         <ul className={css.text}>
           {content.map((text, index) => (
             <li key={index} className={css.listItem}>
--- src/components/FaqAccordion/FaqAccordion.jsx
+++ src/components/FaqAccordion/FaqAccordion.jsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import clsx from "clsx";
 import css from "./FaqAccordion.module.css";
 import AccordionButton from "../UI/AccordionButton/AccordionButton";
 
 const FaqAccordion = ({ faqListData }) => {
   const [openIndex, setOpenIndex] = useState(null);
+  const refs = useRef([]);
 
   const toggleItem = (index) => {
     setOpenIndex(openIndex === index ? null : index);
@@ -14,16 +15,27 @@ const FaqAccordion = ({ faqListData }) => {
     <ul className={css.container}>
       {faqListData.map(({ question, answer }, index) => (
         <li key={index} className={css.item}>
-          <div className={css.header}>
-            <h3 onClick={() => toggleItem(index)}>{question}</h3>
+          <h3 className={css.header} onClick={() => toggleItem(index)}>
+            {question}
             <AccordionButton
               iconId="icon-dropdown"
               isOpen={openIndex === index}
               onClick={() => toggleItem(index)}
             />
-          </div>
+          </h3>
 
-          <div className={clsx(css.panel, openIndex === index && css.active)}>
+          <div
+            ref={(el) => (refs.current[index] = el)}
+            className={clsx(css.panel, openIndex === index && css.active)}
+            style={{
+              maxHeight:
+                openIndex === index
+                  ? `${refs.current[index]?.scrollHeight}px`
+                  : "0",
+              opacity: openIndex === index ? "1" : "0",
+              transition: "max-height 1s ease, opacity 0.5s ease",
+            }}
+          >
             <p className={css.text}>{answer}</p>
           </div>
         </li>
