280be5b53c15dc734b91be5ed12a243d1d64b836
VPresich <valya.alexandrova.presich@gmail.com>
2024-08-31 17:01:00 +0200

    added change background-color for logo component

--- src/components/SplashScreen/SplashScreen.jsx
+++ src/components/SplashScreen/SplashScreen.jsx
@@ -1,13 +1,17 @@
+import { useSelector } from "react-redux";
+import clsx from "clsx";
+import { selectTheme } from "../../redux/theme/selectors";
 import Logo from "../Logo/Logo";
 import CoversSection from "../CoverSection/CoverSection";
 import { projectsUrl } from "../../data/projects-data";
 import css from "./SplashScreen.module.css";
 
 const SplashScreen = () => {
+  const theme = useSelector(selectTheme);
   return (
     <div className={css.container}>
       <CoversSection projects={projectsUrl} />
-      <div className={css.logoContainer}>
+      <div className={clsx(css.logoContainer, css[theme])}>
         <Logo />
       </div>
     </div>
